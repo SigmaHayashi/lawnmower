737135eb17f77315db923dad91c06d6b37cf2c7b
Takuma Hayashi <t_hayashi@irvs.ait.kyushu-u.ac.jp>
2020-09-20 20:44:49 +0900

    virtual_vehicle_controllerを追加 実機なしにodomを出すノード

--- src/virtual_vehicle_controller.cpp
+++ src/virtual_vehicle_controller.cpp
@@ -13,6 +13,7 @@ const double distance_per_rot = 14.06 / 1000; // モーター1回転で進む距
 const double distance_wheel = 0.75;            // 左右輪の距離[m]
 
 // cmd_velに対するコールバック
+/*
 geometry_msgs::Twist cmd_vel;
 void callbackCmdVel(const geometry_msgs::Twist::ConstPtr& msg){
     cmd_vel.linear = msg->linear;
@@ -21,8 +22,10 @@ void callbackCmdVel(const geometry_msgs::Twist::ConstPtr& msg){
     //cmd_vel.linear.x *= 0.5;
     //cmd_vel.angular.z *= 0.5;
 }
+*/
 
 // cmd_velからLawnMowerに送るコマンドを作る
+/*
 lawnmower::command_to_lawnmower makeCommandToLawnmower(){
     lawnmower::command_to_lawnmower msg; // [m/sec] [rad/min]
 
@@ -121,35 +124,176 @@ lawnmower::command_to_lawnmower makeCommandToLawnmower(){
 
     return msg;
 }
+*/
 
 
 // 草刈り機の状態を記録しておく
+/*
 lawnmower::command_from_lawnmower var_command_from_lawnmower;
 void callbackCommandFromLawnmower(const lawnmower::command_from_lawnmower::ConstPtr& msg){
     var_command_from_lawnmower = *msg;
+}
+*/
+
+
+// cmd_vel -> command_to_lawnmower -> command_from_lawnmower -> odom
+// の過程をすっ飛ばす
+nav_msgs::Odometry odom;
+void callbackCmdVel(const geometry_msgs::Twist::ConstPtr& msg){
+    geometry_msgs::Twist cmd_vel;
+    cmd_vel.linear = msg->linear;
+    cmd_vel.angular = msg->angular;
+
+    int cmd_vel_rpm[2];
+    cmd_vel_rpm[0] = (cmd_vel.linear.x - distance_wheel / 2 * cmd_vel.angular.z) * 60 / distance_per_rot;
+    cmd_vel_rpm[1] = (cmd_vel.linear.x + distance_wheel / 2 * cmd_vel.angular.z) * 60 / distance_per_rot;
+    ROS_INFO("cmd_vel_rpm : %d %d", cmd_vel_rpm[0], cmd_vel_rpm[1]);
+
+    int speed_left = 0;
+    int speed_right = 0;
+    if(cmd_vel_rpm[0] > 0){
+        if(cmd_vel_rpm[0] >= (2013 + 1602) / 2){
+            //msg.speed_left = 5;
+            speed_left = 2013;
+        }
+        else if(cmd_vel_rpm[0] >= (1602 + 1109) / 2){
+            //msg.speed_left = 4;
+            speed_left = 1602;
+        }
+        else if(cmd_vel_rpm[0] >= (1109 + 673) / 2){
+            //msg.speed_left = 3;
+            speed_left = 1109;
+        }
+        else if(cmd_vel_rpm[0] >= (673 + 268) / 2){
+            //msg.speed_left = 2;
+            speed_left = 673;
+        }
+        else if(cmd_vel_rpm[0] >= 268 / 2){
+            //msg.speed_left = 1;
+            speed_left = 268;
+        }
+        else{
+            //msg.speed_left = 0;
+            speed_left = 0;
+        }
+    }
+    else if(cmd_vel_rpm[0] < 0){
+        if(cmd_vel_rpm[0] <= (2013 + 1602) / -2){
+            //msg.speed_left = -5;
+            speed_left = -2013;
+        }
+        else if(cmd_vel_rpm[0] <= (1602 + 1109) / -2){
+            //msg.speed_left = -4;
+            speed_left = -1602;
+        }
+        else if(cmd_vel_rpm[0] <= (1109 + 673) / -2){
+            //msg.speed_left = -3;
+            speed_left = -1109;
+        }
+        else if(cmd_vel_rpm[0] <= (673 + 268) / -2){
+            //msg.speed_left = -2;
+            speed_left = -673;
+        }
+        else if(cmd_vel_rpm[0] <= 268 / -2){
+            //msg.speed_left = -1;
+            speed_left = -268;
+        }
+        else{
+            //msg.speed_left = 0;
+            speed_left = 0;
+        }
+    }
+    else{
+        //msg.speed_left = 0;
+        speed_left = 0;
+    }
+
+    if(cmd_vel_rpm[1] > 0){
+        if(cmd_vel_rpm[1] >= (2013 + 1602) / 2){
+            //msg.speed_right = 5;
+            speed_right = 2013;
+        }
+        else if(cmd_vel_rpm[1] >= (1602 + 1109) / 2){
+            //msg.speed_right = 4;
+            speed_right = 1602;
+        }
+        else if(cmd_vel_rpm[1] >= (1109 + 673) / 2){
+            //msg.speed_right = 3;
+            speed_right = 1109;
+        }
+        else if(cmd_vel_rpm[1] >= (673 + 268) / 2){
+            //msg.speed_right = 2;
+            speed_right = 673;
+        }
+        else if(cmd_vel_rpm[1] >= 268 / 2){
+            //msg.speed_right = 1;
+            speed_right = 268;
+        }
+        else{
+            //msg.speed_right = 0;
+            speed_right = 0;
+        }
+    }
+    else if(cmd_vel_rpm[1] < 0){
+        if(cmd_vel_rpm[1] <= (2013 + 1602) / -2){
+            //msg.speed_right = -5;
+            speed_right = -2013;
+        }
+        else if(cmd_vel_rpm[1] <= (1602 + 1109) / -2){
+            //msg.speed_right = -4;
+            speed_right = -1602;
+        }
+        else if(cmd_vel_rpm[1] <= (1109 + 673) / -2){
+            //msg.speed_right = -3;
+            speed_right = -1109;
+        }
+        else if(cmd_vel_rpm[1] <= (673 + 268) / -2){
+            //msg.speed_right = -2;
+            speed_right = -673;
+        }
+        else if(cmd_vel_rpm[1] <= 268 / -2){
+            //msg.speed_right = -1;
+            speed_right = -268;
+        }
+        else{
+            //msg.speed_right = 0;
+            speed_right = 0;
+        }
+    }
+    else{
+        //msg.speed_right = 0;
+        speed_right = 0;
+    }
+
+    double speed_left_mps = speed_left * distance_per_rot / 60;
+    double speed_right_mps = speed_right * distance_per_rot / 60;
+
+    odom.twist.twist.linear.x = (speed_left_mps + speed_right_mps) / 2;
+    odom.twist.twist.angular.z = (speed_right_mps - speed_left_mps) / distance_wheel;
 }
 
 
 // メイン関数
 int main(int argc, char** argv){
 
-    ros::init(argc, argv, "vehicle_controller");
+    ros::init(argc, argv, "virtual_vehicle_controller");
     ros::NodeHandle nh;
 
-    ROS_INFO("Vehicle Controller Start");
+    ROS_INFO("Virtual Vehicle Controller Start");
 
     ros::Subscriber sub_cmd_vel = nh.subscribe("cmd_vel", 1, callbackCmdVel);
-    ros::Publisher pub_command_to_lawnmower = nh.advertise<lawnmower::command_to_lawnmower>("command_to_lawnmower", 1);
+    //ros::Publisher pub_command_to_lawnmower = nh.advertise<lawnmower::command_to_lawnmower>("command_to_lawnmower", 1);
 
-    ros::Subscriber sub_command_from_lawnmofer = nh.subscribe("command_from_lawnmower", 1, callbackCommandFromLawnmower);
+    //ros::Subscriber sub_command_from_lawnmofer = nh.subscribe("command_from_lawnmower", 1, callbackCommandFromLawnmower);
     ros::Publisher pub_odom = nh.advertise<nav_msgs::Odometry>("odom", 100);
 
     ros::Rate loop_rate(20);
 
     // メインループ
     while(ros::ok()){
-        pub_command_to_lawnmower.publish(makeCommandToLawnmower());
+        //pub_command_to_lawnmower.publish(makeCommandToLawnmower());
 
+        /*
         double lawnmower_speed_mps[2];
         lawnmower_speed_mps[0] = var_command_from_lawnmower.speed_left * distance_per_rot / 60;
         lawnmower_speed_mps[1] = var_command_from_lawnmower.speed_right * distance_per_rot / 60;
@@ -157,6 +301,9 @@ int main(int argc, char** argv){
         nav_msgs::Odometry odom;
         odom.twist.twist.linear.x = (lawnmower_speed_mps[0] + lawnmower_speed_mps[1]) / 2;
         odom.twist.twist.angular.z = (lawnmower_speed_mps[1] - lawnmower_speed_mps[0]) / distance_wheel;
+        pub_odom.publish(odom);
+        */
+
         pub_odom.publish(odom);
         
         ros::spinOnce();
